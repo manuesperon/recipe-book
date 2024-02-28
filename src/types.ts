dec7c2d9f8fc892375579faa8519bcd58a007d6f
Manuel Esperon <hello@manuesperon.com>
2024-02-29 10:43:44 +1300

    Add comments to types

--- src/types.ts
+++ src/types.ts
@@ -18,10 +18,12 @@ const recipeWithIngredients = Prisma.validator<Prisma.RecipeDefaultArgs>()({
   },
 });
 
-// Custom Recipet type that includes ingredients, based on query defined in recipeWithIngredients.
+// Custom Recipe type that includes ingredients, based on query defined in recipeWithIngredients.
 export type RecipeWithIngredients = Prisma.RecipeGetPayload<typeof recipeWithIngredients>;
 
 const ingredientWithUnit = Prisma.validator<Prisma.IngredientsOnRecipesDefaultArgs>()({
   select: { amount: true, unit: { select: { abbreviation: true, label: true } }, ingredient: true },
 });
+
+// Custom Ingredient type including its unit and amount for the current recipe.
 export type IngredientWithUnit = Prisma.IngredientsOnRecipesGetPayload<typeof ingredientWithUnit>;
